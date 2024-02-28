9aecaed951c5bf2a9715bfbeba57e8d91e0db8a3
shibatakeigo <sagankeigo17@gmail.com>
2024-02-28 14:19:22 +0900

    Add print on main_controller

--- src/main_controller.py
+++ src/main_controller.py
@@ -17,15 +17,18 @@ class MainController:
         # ログの初期化
         self.log_system = LogSystem()
         self.log_system.write("Success : Init Log system")
+        print("Success : Init Log system")
         
         # UDPの初期化
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind((host_name, port))
         self.log_system.write("Success : Init UDP socket")
+        print("Success : Init UDP socket")
         
         # can通信の初期化
         self.bus = can.interface.Bus(channel='can0', bustype='socketcan', bitrate=5000000)
         self.log_system.write("Success : Init CAN socket")
+        print("Success : Init CAN socket")
         
     def write_can_bus(self, can_id: int, data: bytearray):
         msg = can.Message(arbitration_id=can_id, data=data, is_extended_id=False)
@@ -33,12 +36,14 @@ class MainController:
         
         self.log_system.write("Write CAN Bus : id={}, msg={}".format(can_id, data.hex()))
         self.log_system.write_with_can_id(data.hex(), can_id)
+        print("Write CAN Bus : id={}, msg={}".format(can_id, data.hex()))
         time.sleep(0.01)
         
     def read_udp(self):
         data, addr = self.sock.recvfrom(1024)
         self.log_system.write("Read UDP : data={}, addr={}".format(data, addr))
         self.log_system.write_with_udp_client_name(data, addr.__str__())
+        print("Read UDP : data={}, addr={}".format(data, addr))
         return data
     
     def __del__(self):
