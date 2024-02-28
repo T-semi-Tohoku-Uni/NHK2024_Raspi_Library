cf371ceed7c46484decbcb37a57f389bcf478b87
shibatakeigo <sagankeigo17@gmail.com>
2024-02-28 13:14:26 +0900

    Apply write** func to main_controller

--- src/main_controller.py
+++ src/main_controller.py
@@ -29,16 +29,18 @@ class MainController:
         self.bus = can.interface.Bus(channel='can0', bustype='socketcan', bitrate=5000000)
         self.log_system.write("Success : Init CAN socket")
         
-    def write_can_bus(self, can_id, data: bytearray):
+    def write_can_bus(self, can_id: int, data: bytearray):
         msg = can.Message(arbitration_id=can_id, data=data, is_extended_id=False)
         self.bus.send(msg, timeout=0.01)
         
         self.log_system.write("Write CAN Bus : id={}, msg={}".format(can_id, data.hex()))
+        self.log_system.write_with_can_id(data.hex(), can_id)
         time.sleep(0.01)
         
     def read_udp(self):
         data, addr = self.sock.recvfrom(1024)
         self.log_system.write("Read UDP : data={}, addr={}".format(data, addr))
+        self.log_system.write_with_udp_client_name(data, addr.__str__())
         return data
     
     @abstractmethod
